fbd333a0f9703c2ab59ffa8af61c8d44d8abe629
Rajat Kumar <rajatk12199@gmail.com>
2021-12-04 17:28:52 +1100

    cleanup

--- src/buttons.js
+++ src/buttons.js
@@ -2,20 +2,20 @@ const button = [
     { id: 'one', value: 1 },
     { id: 'two', value: 2 },
     { id: 'three', value: 3 },
-    { id: 'clear', value: 'AC' },
     { id: 'four', value: 4 },
     { id: 'five', value: 5 },
     { id: 'six', value: 6 },
-    { id: 'add', value: '+' },
-    { id: 'subtract', value: '-' },
     { id: 'seven', value: 7 },
     { id: 'eight', value: 8 },
     { id: 'nine', value: 9 },
+    { id: 'zero', value: 0 },
+    { id: 'add', value: '+' },
+    { id: 'subtract', value: '-' },
     { id: 'multiply', value: 'x' },
     { id: 'divide', value: '/' },
     { id: 'decimal', value: '.' },
-    { id: 'zero', value: 0 },
-    { id: 'equals', value: '=' }
+    { id: 'equals', value: '=' },
+    { id: 'clear', value: 'AC' }
 ]
 
 export default button
