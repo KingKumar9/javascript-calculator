43a4650a66eec9aa80086ec04b2e48105e28c48f
Rajat Kumar <rajatk12199@gmail.com>
2021-12-04 17:27:13 +1100

    logic first build

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import React from 'react';
+import buttons from './buttons.js';
 import './App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      display: 0
+    }
+    this.numToDisplay = this.numToDisplay.bind(this)
+  }
+
+  numToDisplay(e) {
+    const value = e.target.innerText
+    if (this.state.display === 0 && parseInt(value) === 0) {
+      this.setState({
+        display: 0
+      })
+    } else if (this.state.display === 0) {
+      this.setState({
+        display: [value]
+      })
+    } else {
+      this.setState({
+        display: [...this.state.display, value]
+      })
+    }
+  }
+
   render() {
     return (
       <div className="container">
         <div className="calculator">
-          
+          <div id="display">
+            <h2 className="equation">{ this.state.display }</h2>
+          </div>
+          <div className="buttons">
+            { buttons.map(i => { return <button onClick={ this.numToDisplay } id={ i.id }>{ i.value }</button> }) }
+          </div>
         </div>
       </div>
     )
